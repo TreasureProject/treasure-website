6b8ece810ac42faab9a1b7fb699746f4af4ce6b6
0xkarel <95452264+0xkarel@users.noreply.github.com>
2022-06-12 21:17:50 -0400

    Update metadata

--- app/routes/community.tsx
+++ app/routes/community.tsx
@@ -19,7 +19,7 @@ export const meta: MetaFunction = ({ parentsData }) => {
   return {
     ...getSocialMetas({
       description:
-        "Treasure is a decentralized video game console connecting games and communities together through imagination, $MAGIC, and NFTs.",
+        "Treasure is the decentralized video game console connecting games and communities together through imagination, $MAGIC, and NFTs.",
       keywords: "treasure, NFT, DeFi, games, community, imagination, magic",
       title: generateTitle("/community"),
       origin: requestInfo.origin,
