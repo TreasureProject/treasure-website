90e804f72a47b36a0f3a7eed6a9194702f5476fa
Yuya Oiwa <yuya.oiwa@me.com>
2022-07-08 13:37:57 +0900

    Revert "trying out"
    This reverts commit bcdeb49b360cc6be57a00179b07d1f9ebd5657e6.

--- app/utils/sanity/getClient.ts
+++ app/utils/sanity/getClient.ts
@@ -3,17 +3,13 @@ import type { CloudFlareEnv } from "../../types";
 import type { PicoSanity as PicoSanityT } from "picosanity";
 import { CONFIG } from "./config";
 
-const client = new PicoSanity(CONFIG);
-
-let previewClient: PicoSanityT | null = null;
-
 export const getClient = (usePreview = false, env: CloudFlareEnv) => {
-  if (!previewClient) {
-    previewClient = new PicoSanity({
-      ...CONFIG,
-      token: env.SANITY_API_TOKEN ?? ``,
-    });
-  }
+  const client = new PicoSanity(CONFIG);
+
+  const previewClient = new PicoSanity({
+    ...CONFIG,
+    token: env.SANITY_API_TOKEN ?? ``,
+  });
 
   return usePreview ? previewClient : client;
 };
