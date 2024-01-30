9ce3132712ccbbb37987631b59e41569b79d34d3
BasVanDeGroep <accounts@basvandegroep.com>
2024-01-30 15:29:45 +0100

    set text to white on entire landing page

--- app/components/new-landing/NewLayout.tsx
+++ app/components/new-landing/NewLayout.tsx
@@ -6,7 +6,7 @@ const DEV = false;
 
 export const NewLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative max-w-[100vw] overflow-x-hidden bg-new-night-1300">
+    <main className="relative max-w-[100vw] overflow-x-hidden bg-new-night-1300 text-new-night-100">
       {DEV && (
         <div className="fixed top-10 left-10 z-[999] bg-white p-4 font-black text-black">
           <p className="hidden 2xl:block">2XL</p>
--- app/components/new-landing/sections/Games.tsx
+++ app/components/new-landing/sections/Games.tsx
@@ -4,81 +4,91 @@ import type { SwiperClass } from "swiper/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard } from "swiper/modules";
 
-import BEACON_COVER from "~/../public/img/new-landing/game-covers/The_Beacon_Game_Cover.png";
-import BEACON_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Beacon_Game_Background.png";
+import BEACON_COVER from "~/../public/img/new-landing/game-covers/The_Beacon_Game_Cover.webp";
+import BEACON_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Beacon_Game_Background.webp";
 
-import KAIJU_CARDS_COVER from "~/../public/img/new-landing/game-covers/The_Kaiju_Cards_Game_Cover.png";
-import KAIJU_CARDS_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Kaiju_Cards_Game_Background.png";
+import KAIJU_CARDS_COVER from "~/../public/img/new-landing/game-covers/The_Kaiju_Cards_Game_Cover.webp";
+import KAIJU_CARDS_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Kaiju_Cards_Game_Background.webp";
 
-import KOTE_COVER from "~/../public/img/new-landing/game-covers/The_KOTE_Game_Cover.png";
-import KOTE_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_KOTE_Game_Background.png";
+import KOTE_COVER from "~/../public/img/new-landing/game-covers/The_KOTE_Game_Cover.webp";
+import KOTE_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_KOTE_Game_Background.webp";
 
-import KURORO_BEAST_COVER from "~/../public/img/new-landing/game-covers/The_Kuroro_Game_Cover_2.png";
-import KURORO_BEAST_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Kuroro_Game_Background.png";
+import KURORO_BEAST_COVER from "~/../public/img/new-landing/game-covers/The_Kuroro_Game_Cover_2.webp";
+import KURORO_BEAST_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Kuroro_Game_Background.webp";
 
-import MAH_COVER from "~/../public/img/new-landing/game-covers/The_MAH_Game_Cover.png";
-import MAH_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_MAH_Game_Background.png";
+import MAH_COVER from "~/../public/img/new-landing/game-covers/The_MAH_Game_Cover.webp";
+import MAH_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_MAH_Game_Background.webp";
 
-import ZEEVERSE_COVER from "~/../public/img/new-landing/game-covers/The_Zeeverse_Game_Cover.png";
-import ZEEVERSE_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Zeeverse_Game_Background.png";
+import ZEEVERSE_COVER from "~/../public/img/new-landing/game-covers/The_Zeeverse_Game_Cover.webp";
+import ZEEVERSE_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Zeeverse_Game_Background.webp";
 
-import BITMATES_COVER from "~/../public/img/new-landing/game-covers/The_Bitmates_Game_Cover.png";
-import BITMATES_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Bitmates_Game_Background.png";
+import BITMATES_COVER from "~/../public/img/new-landing/game-covers/The_Bitmates_Game_Cover.webp";
+import BITMATES_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Bitmates_Game_Background.webp";
 
-import REALM_COVER from "~/../public/img/new-landing/game-covers/The_Realm_Game_Cover.png";
-import REALM_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Realm_Game_Background.png";
+import REALM_COVER from "~/../public/img/new-landing/game-covers/The_Realm_Game_Cover.webp";
+import REALM_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_Realm_Game_Background.webp";
 
-import TOE_COVER from "~/../public/img/new-landing/game-covers/The_Toe_Game_Cover.png";
-import TOE_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_ToE_Game_Background.png";
+import TOE_COVER from "~/../public/img/new-landing/game-covers/The_Toe_Game_Cover.webp";
+import TOE_BACKGROUND from "~/../public/img/new-landing/game-backgrounds/The_ToE_Game_Background.webp";
 
 import { twMerge } from "tailwind-merge";
 import { ChevronRightIcon } from "../misc/Icons";
+import { GamesGrid } from "../misc/Svgs";
 
 const games = [
   {
     name: "Beacon",
     cover: BEACON_COVER,
     background: BEACON_BACKGROUND,
+    link: "https://app.treasure.lol/games/the-beacon",
   },
   {
     name: "Kaiju Cards",
     cover: KAIJU_CARDS_COVER,
     background: KAIJU_CARDS_BACKGROUND,
+    link: "https://app.treasure.lol/games/kaiju-cards",
   },
   {
     name: "KOTE",
     cover: KOTE_COVER,
     background: KOTE_BACKGROUND,
+    link: "https://app.treasure.lol/games/kote",
   },
   {
     name: "Kuroro Beast",
     cover: KURORO_BEAST_COVER,
     background: KURORO_BEAST_BACKGROUND,
+    link: "https://app.treasure.lol/games/kuroro-beasts",
   },
   {
     name: "MAH",
     cover: MAH_COVER,
     background: MAH_BACKGROUND,
+    link: "https://app.treasure.lol/games/mighty-action-heroes",
   },
   {
     name: "Zeeverse",
     cover: ZEEVERSE_COVER,
     background: ZEEVERSE_BACKGROUND,
-  },
-  {
-    name: "Bitmates",
-    cover: BITMATES_COVER,
-    background: BITMATES_BACKGROUND,
-  },
-  {
-    name: "Realm",
-    cover: REALM_COVER,
-    background: REALM_BACKGROUND,
+    link: "https://app.treasure.lol/games/zeeverse",
   },
   {
     name: "TOE",
     cover: TOE_COVER,
     background: TOE_BACKGROUND,
+    link: "https://app.treasure.lol/games/tales-of-elleria",
+  },
+  {
+    name: "Bitmates",
+    cover: BITMATES_COVER,
+    background: BITMATES_BACKGROUND,
+    link: "https://app.treasure.lol/games/bitmates",
+  },
+  {
+    name: "Realm",
+    cover: REALM_COVER,
+    background: REALM_BACKGROUND,
+    link: "https://app.treasure.lol/games/realm",
   },
 ] as const;
 
@@ -173,7 +183,15 @@ const Games = () => {
           {games.map((game, i) => {
             return (
               <SwiperSlide key={game.name}>
-                <a href="/" target="_blank" rel="noreferrer">
+                <a
+                  href="/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className={twMerge(
+                    "overflow-hidden rounded-lg border-2 border-new-night-700",
+                    activeGame === i && "border-new-night-500"
+                  )}
+                >
                   <img
                     className="h-full w-full object-cover object-center"
                     alt={gameByIndex(i).name}
@@ -185,25 +203,12 @@ const Games = () => {
           })}
         </Swiper>
       </div>
-
-      {/* Navigation dots */}
-      <div className="container relative z-10 mt-12 flex w-full items-center justify-center gap-2">
-        {games.map((game, index) => (
-          <button
-            key={game.name}
-            className={twMerge(
-              "h-1 w-8 rounded-full bg-night-800",
-              index === activeGame && "bg-new-night-100"
-            )}
-            onClick={() => setActiveGame(index)}
-          />
-        ))}
-      </div>
-
+      <div className="to-transaprent absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-new-night-1300" />
+      <GamesGrid className="absolute inset-0 z-[5] min-h-[600px] opacity-25" />
       {/* View All Games link */}
       <Link
         to="/games"
-        className="relative z-10 mt-8 flex w-full items-center justify-center gap-1.5 font-semibold leading-[160%]"
+        className="relative z-10 mt-8 flex w-full items-center justify-center gap-1.5 font-semibold leading-[160%] transition-opacity hover:opacity-75"
       >
         View All Games <ChevronRightIcon className="w-4" />
       </Link>
