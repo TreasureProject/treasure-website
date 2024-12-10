70a00a712642cd7629af6b7234943d252fbe4ea3
Karel Vuong <95452264+karelvuong@users.noreply.github.com>
2024-12-09 23:11:47 -0500

    add remix-sitemap

--- app/routes/action.webhook.tsx
+++ app/routes/action.webhook.tsx
@@ -2,6 +2,11 @@ import type { ActionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { badRequest } from "remix-utils";
 import { contenfulDeliverySdk } from "~/utils/client.server";
+import type { SitemapFunction } from "remix-sitemap";
+
+export const sitemap: SitemapFunction = () => ({
+  exclude: true,
+});
 
 const BASE_URL = "https://treasure.lol/blog";
 
